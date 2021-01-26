f65cd3a71c18af65bf90245f06ac97f1c8c865fd
Reygan123 <fadhilahreygan@yahoo.com>
2021-01-26 12:34:45 +0700

    bab 2

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Login Multi User Dengan PHP Dan Mysqli</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>Membuat Login Multi User Dengan PHP Dan Mysqli</h1>
+<?php
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan']=="gagal"){
+			echo"<div class='alert'>Username dan Password tidak sesuai!</div>";
+		}
+	}
+?>
+<div class="kotak login">
+	<p class="tulisan_login">Silahkan login</p>
+	<form action="cek_login.php" method="post">
+		<label>Username</label>
+	<input type="text" name="username" class="form_login" placeholder="Username.." required="required">
+		<label>Password</label>
+	<input type="password" name="password" class="form_login" placeholder="Password.." required="required">
+		<input type="submit" class="tombol_login" value="LOGIN">
+		<br/>
+		<br/>
+	 </form>
+</div>
+</body>
+</html>
